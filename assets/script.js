e5c1cd34ed33bfc4864b7317fb5b0e19e52c922e
K4ne <nick@getmighty.net>
2022-04-07 16:26:21 -0500

    Added to readme and added screenshot

--- assets/script.js
+++ assets/script.js
@@ -1,7 +1,7 @@
 // Declaring variables
 time = document.querySelector("#time");
-nineam = document.getElementById("9amtextarea");
-tenam = document.getElementById("10amtextarea");
+nineAm = document.getElementById("9amtextarea");
+tenAm = document.getElementById("10amtextarea");
 elevenam = document.getElementById("11amtextarea");
 twelvepm = document.getElementById("12pmtextarea");
 onepm = document.getElementById("1pmtextarea");
@@ -9,23 +9,30 @@ twopm = document.getElementById("2pmtextarea");
 threepm = document.getElementById("3pmtextarea");
 fourpm = document.getElementById("4pmtextarea");
 fivepm = document.getElementById("5pmtextarea");
-geteverything();
+// Displaying all storage onto page.
+nineAm.value = localStorage.getItem("9am")
+tenAm.value = localStorage.getItem("10am")
+elevenam.value = localStorage.getItem("11am")
+twelvepm.value = localStorage.getItem("12pm")
+onepm.value = localStorage.getItem("1pm")
+twopm.value = localStorage.getItem("2pm")
+threepm.value = localStorage.getItem("3pm")
+fourpm.value = localStorage.getItem("4pm")
+fivepm.value = localStorage.getItem("5pm")
 
+// Repeating every 1s
 setInterval(function() {
     time.innerText = moment().format("MMM DD, YYYY [at] hh:mm:ss a");
 }, 1000);
 
 
 
-
+// If statements for past
 if (moment().hours() > 9) {
-    nineam.classList.add("bg-secondary");
-}
-else {
-    ninepm.classList.add("bg-danger");
+    nineAm.classList.add("bg-secondary");
 }
 if (moment().hours() > 10) {
-    tenam.classList.add("bg-secondary");
+    tenAm.classList.add("bg-secondary");
 }
 if (moment().hours() > 11) {
     elevenam.classList.add("bg-secondary");
@@ -48,12 +55,12 @@ if (moment().hours() > 16) {
 if (moment().hours() > 17) {
     fivepm.classList.add("bg-secondary");
 }
-
+// If statements for future
 if (moment().hours() < 9) {
-    nineam.classList.add("bg-danger");
+    nineAm.classList.add("bg-danger");
 }
 if (moment().hours() < 10) {
-    tenam.classList.add("bg-danger");
+    tenAm.classList.add("bg-danger");
 }
 if (moment().hours() < 11) {
     elevenam.classList.add("bg-danger");
@@ -78,13 +85,12 @@ if (moment().hours() < 17) {
 }
 
 
-
-
+// If statements for current time
 if (moment().hours() == 9) {
-    nineam.classList.add("bg-success");
+    nineAm.classList.add("bg-success");
 }
 if (moment().hours() == 10) {
-    tenam.classList.add("bg-success");
+    tenAm.classList.add("bg-success");
 }
 if (moment().hours() == 11) {
     elevenam.classList.add("bg-success");
@@ -108,57 +114,44 @@ if (moment().hours() == 17) {
     fivepm.classList.add("bg-success");
 }
 
-console.log(moment().hours() == 15)
-
 
 // Save everything 
 function save9am(){
-    console.log(nineam.value)
-    localStorage.setItem("9am",nineam.value)
+    console.log(nineAm.value)
+    localStorage.setItem("9am",nineAm.value)
 }
 function save10am(){
-    console.log(nineam.value)
-    localStorage.setItem("10am",tenam.value)
+    console.log(nineAm.value)
+    localStorage.setItem("10am",tenAm.value)
 }
 function save11am(){
-    console.log(nineam.value)
+    console.log(nineAm.value)
     localStorage.setItem("11am",elevenam.value)
 }
 function save12pm(){
-    console.log(nineam.value)
+    console.log(nineAm.value)
     localStorage.setItem("12pm",twelvepm.value)
 }
 function save1pm(){
-    console.log(nineam.value)
+    console.log(nineAm.value)
     localStorage.setItem("1pm",onepm.value)
 }
 function save2pm(){
-    console.log(nineam.value)
+    console.log(nineAm.value)
     localStorage.setItem("2pm",twopm.value)
 }
 function save3pm(){
-    console.log(nineam.value)
+    console.log(nineAm.value)
     localStorage.setItem("3pm",threepm.value)
 }
 function save4pm(){
-    console.log(nineam.value)
+    console.log(nineAm.value)
     localStorage.setItem("4pm",fourpm.value)
 }
 function save5pm(){
-    console.log(nineam.value)
+    console.log(nineAm.value)
     localStorage.setItem("5pm",fivepm.value)
 }
 // Save everything end
-function geteverything () 
-{
-nineam.value = localStorage.getItem("9am")
-tenam.value = localStorage.getItem("10am")
-elevenam.value = localStorage.getItem("11am")
-twelvepm.value = localStorage.getItem("12pm")
-onepm.value = localStorage.getItem("1pm")
-twopm.value = localStorage.getItem("2pm")
-threepm.value = localStorage.getItem("3pm")
-fourpm.value = localStorage.getItem("4pm")
-fivepm.value = localStorage.getItem("5pm")
-}
+
 
