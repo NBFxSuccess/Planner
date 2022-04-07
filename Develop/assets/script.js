cb65f15848900f2c808343cf6b1368b642d17339
K4ne <nick@getmighty.net>
2022-04-07 16:08:26 -0500

    copied pasted the first box I had done and added time if statements for color

--- Develop/assets/script.js
+++
@@ -1,4 +0,0 @@
-time = document.querySelector("#time");
-setInterval(function() {
-    time.innerText = moment().format("MMM DD, YYYY [at] hh:mm:ss a");
-}, 1000);
---
+++ assets/script.js
@@ -0,0 +1,164 @@
+// Declaring variables
+time = document.querySelector("#time");
+nineam = document.getElementById("9amtextarea");
+tenam = document.getElementById("10amtextarea");
+elevenam = document.getElementById("11amtextarea");
+twelvepm = document.getElementById("12pmtextarea");
+onepm = document.getElementById("1pmtextarea");
+twopm = document.getElementById("2pmtextarea");
+threepm = document.getElementById("3pmtextarea");
+fourpm = document.getElementById("4pmtextarea");
+fivepm = document.getElementById("5pmtextarea");
+geteverything();
+
+setInterval(function() {
+    time.innerText = moment().format("MMM DD, YYYY [at] hh:mm:ss a");
+}, 1000);
+
+
+
+
+if (moment().hours() > 9) {
+    nineam.classList.add("bg-secondary");
+}
+else {
+    ninepm.classList.add("bg-danger");
+}
+if (moment().hours() > 10) {
+    tenam.classList.add("bg-secondary");
+}
+if (moment().hours() > 11) {
+    elevenam.classList.add("bg-secondary");
+}
+if (moment().hours() > 12) {
+    twelvepm.classList.add("bg-secondary");
+}
+if (moment().hours() > 13) {
+    onepm.classList.add("bg-secondary");
+}
+if (moment().hours() > 14) {
+    twopm.classList.add("bg-secondary");
+}
+if (moment().hours() > 15) {
+    threepm.classList.add("bg-secondary");
+}
+if (moment().hours() > 16) {
+    fourpm.classList.add("bg-secondary");
+}
+if (moment().hours() > 17) {
+    fivepm.classList.add("bg-secondary");
+}
+
+if (moment().hours() < 9) {
+    nineam.classList.add("bg-danger");
+}
+if (moment().hours() < 10) {
+    tenam.classList.add("bg-danger");
+}
+if (moment().hours() < 11) {
+    elevenam.classList.add("bg-danger");
+}
+if (moment().hours() < 12) {
+    twelvepm.classList.add("bg-danger");
+}
+if (moment().hours() < 13) {
+    onepm.classList.add("bg-danger");
+}
+if (moment().hours() < 14) {
+    twopm.classList.add("bg-danger");
+}
+if (moment().hours() < 15) {
+    threepm.classList.add("bg-danger");
+}
+if (moment().hours() < 16) {
+    fourpm.classList.add("bg-danger");
+}
+if (moment().hours() < 17) {
+    fivepm.classList.add("bg-danger");
+}
+
+
+
+
+if (moment().hours() == 9) {
+    nineam.classList.add("bg-success");
+}
+if (moment().hours() == 10) {
+    tenam.classList.add("bg-success");
+}
+if (moment().hours() == 11) {
+    elevenam.classList.add("bg-success");
+}
+if (moment().hours() == 12) {
+    twelvepm.classList.add("bg-success");
+}
+if (moment().hours() == 13) {
+    onepm.classList.add("bg-success");
+}
+if (moment().hours() == 14) {
+    twopm.classList.add("bg-success");
+}
+if (moment().hours() == 15) {
+    threepm.classList.add("bg-success");
+}
+if (moment().hours() == 16) {
+    fourpm.classList.add("bg-success");
+}
+if (moment().hours() == 17) {
+    fivepm.classList.add("bg-success");
+}
+
+console.log(moment().hours() == 15)
+
+
+// Save everything 
+function save9am(){
+    console.log(nineam.value)
+    localStorage.setItem("9am",nineam.value)
+}
+function save10am(){
+    console.log(nineam.value)
+    localStorage.setItem("10am",tenam.value)
+}
+function save11am(){
+    console.log(nineam.value)
+    localStorage.setItem("11am",elevenam.value)
+}
+function save12pm(){
+    console.log(nineam.value)
+    localStorage.setItem("12pm",twelvepm.value)
+}
+function save1pm(){
+    console.log(nineam.value)
+    localStorage.setItem("1pm",onepm.value)
+}
+function save2pm(){
+    console.log(nineam.value)
+    localStorage.setItem("2pm",twopm.value)
+}
+function save3pm(){
+    console.log(nineam.value)
+    localStorage.setItem("3pm",threepm.value)
+}
+function save4pm(){
+    console.log(nineam.value)
+    localStorage.setItem("4pm",fourpm.value)
+}
+function save5pm(){
+    console.log(nineam.value)
+    localStorage.setItem("5pm",fivepm.value)
+}
+// Save everything end
+function geteverything () 
+{
+nineam.value = localStorage.getItem("9am")
+tenam.value = localStorage.getItem("10am")
+elevenam.value = localStorage.getItem("11am")
+twelvepm.value = localStorage.getItem("12pm")
+onepm.value = localStorage.getItem("1pm")
+twopm.value = localStorage.getItem("2pm")
+threepm.value = localStorage.getItem("3pm")
+fourpm.value = localStorage.getItem("4pm")
+fivepm.value = localStorage.getItem("5pm")
+}
+
