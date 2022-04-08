55c3f099cd3afc678830ce011856b63357d9eb73
K4ne <nick@getmighty.net>
2022-04-07 22:11:14 -0500

    reverted refactor chunk due to lack of functionality

--- assets/script.js
+++ assets/script.js
@@ -2,30 +2,30 @@
 let time = document.querySelector("#time");
 let nineAm = document.getElementById("9amtextarea");
 let tenAm = document.getElementById("10amtextarea");
-let elevenam = document.getElementById("11amtextarea");
-let twelvepm = document.getElementById("12pmtextarea");
-let onepm = document.getElementById("1pmtextarea");
-let twopm = document.getElementById("2pmtextarea");
+let elevenAm = document.getElementById("11amtextarea");
+let twelvePm = document.getElementById("12pmtextarea");
+let onePm = document.getElementById("1pmtextarea");
+let twoPm = document.getElementById("2pmtextarea");
 let threepm = document.getElementById("3pmtextarea");
-let fourpm = document.getElementById("4pmtextarea");
-let fivepm = document.getElementById("5pmtextarea");
+let fourPm = document.getElementById("4pmtextarea");
+let fivePm = document.getElementById("5pmtextarea");
 // Displaying all storage onto page.
 nineAm.value = localStorage.getItem("9am")
 tenAm.value = localStorage.getItem("10am")
-elevenam.value = localStorage.getItem("11am")
-twelvepm.value = localStorage.getItem("12pm")
-onepm.value = localStorage.getItem("1pm")
-twopm.value = localStorage.getItem("2pm")
+elevenAm.value = localStorage.getItem("11am")
+twelvePm.value = localStorage.getItem("12pm")
+onePm.value = localStorage.getItem("1pm")
+twoPm.value = localStorage.getItem("2pm")
 threepm.value = localStorage.getItem("3pm")
-fourpm.value = localStorage.getItem("4pm")
-fivepm.value = localStorage.getItem("5pm")
+fourPm.value = localStorage.getItem("4pm")
+fivePm.value = localStorage.getItem("5pm")
 
 // Repeating every 1s
 setInterval(function() {
     time.innerText = moment().format("MMM DD, YYYY [at] hh:mm:ss a");
 }, 1000);
 
-const times = ["","","","","","","","","",nineAm,tenAm,elevenam,twelvepm,onepm,twopm,threepm,fourpm,fivepm]
+const times = ["","","","","","","","","",nineAm,tenAm,elevenAm,twelvePm,onePm,twoPm,threepm,fourPm,fivePm]
 
 
 //sets all to secondary collar
@@ -40,7 +40,7 @@ for (let index = 9; index < 17; index++) {
        times[index].classList.add("bg-success")
     }
 }
-// rewrites over secondary if moment hours = more than curren
+// rewrites over secondary if moment hours = more than current
 for (let index = 9; index < 18; index++) {
     
     if (moment().hours() < index) {
@@ -48,17 +48,39 @@ for (let index = 9; index < 18; index++) {
     }
 }
 
-function save() {
-
-    localStorage.setItem("9am",nineAm.value)
-    localStorage.setItem("10am",elevenam.value)
-    localStorage.setItem("11am",elevenam.value)
-    localStorage.setItem("12pm",twelvepm.value)
-    localStorage.setItem("1pm",onepm.value)
-    localStorage.setItem("2pm",twopm.value)
-    localStorage.setItem("3pm",threepm.value)
-    localStorage.setItem("4pm",fourpm.value)
-    localStorage.setItem("5pm",fivepm.value)
-}
-    
-
+    function save9am()
+    {
+        localStorage.setItem("9am",nineAm.value)
+    }
+    function save10am()
+    {
+        localStorage.setItem("10am",tenAm.value)
+    }
+    function save11am()
+    {
+        localStorage.setItem("11am",elevenAm.value)
+    }
+    function save12pm()
+    {
+        localStorage.setItem("12pm",twelvePm.value)
+    }
+    function save1pm()
+    {
+        localStorage.setItem("1pm",onePm.value)
+    }
+    function save2pm()
+    {
+        localStorage.setItem("2pm",twoPm.value)
+    }
+    function save3pm()
+    {
+        localStorage.setItem("3pm",threepm.value)
+    }
+    function save4pm()
+    {
+        localStorage.setItem("4pm",fourPm.value)
+    }
+    function save5pm()
+    {
+        localStorage.setItem("5pm",fivePm.value)
+    }
